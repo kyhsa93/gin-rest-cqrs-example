0fefcfe49094157d1b63778398dbab37722d6c49
kyhsa93 <kyhsa93@naver.com>
2019-12-13 22:48:09 +0900

    Update Study Delete Swagger

--- study/router/delete.go
+++ study/router/delete.go
@@ -6,6 +6,11 @@ import (
 	"go-rest-example/study/repository"
 )
 
+// @Description delete study by id
+// @Tags Studies
+// @Param id path string true "Study ID"
+// @Success 200
+// @Router /studies/{id} [delete]
 func Delete(context *gin.Context, repository repository.Repository) {
 	controller.Delete(context.Param("id"), repository)
 }
