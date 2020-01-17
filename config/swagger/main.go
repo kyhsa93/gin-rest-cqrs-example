b5acd340f92db5947b025ee7f215f8cbcf599c03
kyhsa93 <kyhsa93@naver.com>
2020-01-17 19:22:30 +0900

    Modify Swagger title and description

--- config/swagger/main.go
+++ config/swagger/main.go
@@ -9,8 +9,8 @@ type Swagger struct{}
 
 // NewSwagger create swagger configuration instance
 func NewSwagger() *Swagger {
-	docs.SwaggerInfo.Title = "Go REST api Example"
-	docs.SwaggerInfo.Description = "This is Example for REST api using Go"
+	docs.SwaggerInfo.Title = "Gin REST api Example"
+	docs.SwaggerInfo.Description = "This is Example for REST api using Gin"
 	docs.SwaggerInfo.Version = "1.0.0"
 	docs.SwaggerInfo.Host = "localhost:5000"
 	return &Swagger{}
