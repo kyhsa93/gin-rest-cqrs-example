97cb7f792a37bf14ec53ea4da2b08bc7bc233d15
kyhsa93 <kyhsa93@naver.com>
2020-03-11 20:21:23 +0900

    Remove AccountID from POST /profiles body data

--- profile/body/main.go
+++ profile/body/main.go
@@ -2,7 +2,6 @@ package body
 
 // CreateProfile request body for POST /profiles
 type CreateProfile struct {
-	AccountID             string   `json:"accountId" example:"accountId"`
 	Email                 string   `json:"email" example:"test@gmail.com"`
 	Gender                string   `json:"gender" example:"male"`
 	InterestedField       string   `json:"interestedField" example:"develop"`
